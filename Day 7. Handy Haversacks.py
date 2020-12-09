c67085ac5aa0dc9bbc69963d4b62fea6851ae772
Kaysmink <44898676+Kaysmink@users.noreply.github.com>
2020-12-09 21:41:41 +0100

    Add files via upload

--- Day 7. Handy Haversacks.py
+++ Day 7. Handy Haversacks.py
@@ -11,6 +11,8 @@ Input=open("day 7. input.txt", "r").read().split("\n")[:-1]
 bagDictionary = defaultdict(list) 
 searching = "shiny gold"
 
+
+# deel 1  
 for line in Input: 
     line = line.split("bags contain ")
     headBag = line[0].strip()
@@ -20,7 +22,6 @@ for line in Input:
         bagDictionary[headBag].append(bag[1].replace(" bags", "").replace(" bag", "").strip())
     
 allColours = list(bagDictionary.keys())
-
 possible = [colour for colour in allColours if searching in bagDictionary[colour]]
 
 while True:
@@ -38,5 +39,69 @@ while True:
 print(len(possible))
 
 
+# deel 2 
+# opnieuw inlezen aangezien ik delen heb overgeslagen welke niet nodig waren in 1 
+bagDictionary = defaultdict(lambda: defaultdict(int)) 
+for line in Input: 
+    contentDict = defaultdict() 
+    line = line.split("bags contain ")
+    headBag = line[0].strip()
+    content = [x.strip().replace(".", "") for x in line[1].strip().split(",")]
+    for bag in content:
+        bag = bag.split(" ", 1)
+        contentDict[bag[1].replace(" bags", "").replace(" bag", "").strip()] = bag[0]
+        bagDictionary[headBag] = contentDict
+
+emptyBags = [bags for bags in allColours if "other" in bagDictionary[bags].keys()]
+numOfBagsDict = defaultdict(int)
+
+bagDictionary["shiny gold"]
+
+def countBags(colour):
+    Total = 1
+    
+    if colour in emptyBags: 
+        return Total
+    
+    for key, value in bagDictionary[colour].items(): 
+        Total = Total + (countBags(key) * int(value))
+        
+    return Total
+
+
+print(countBags(searching) - 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
     
